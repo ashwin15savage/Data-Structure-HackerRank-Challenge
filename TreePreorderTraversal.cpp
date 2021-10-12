929f4a191982b1610af1a5d46d7cae62567cb2a5
Ashwin24 <68583898+ashwin15savage@users.noreply.github.com>
2021-10-12 18:27:53 +0530

    Delete TreePreorderTraversal.cpp

--- TreePreorderTraversal.cpp
+++
@@ -1,7 +0,0 @@
-void preOrder(Node *root) {
- if(root)
-         {cout<<root->data<<" ";
-                 preOrder(root->left);
-                         preOrder(root->right);
-                                 }
-    }
